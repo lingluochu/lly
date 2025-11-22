0f4167e074e83a3a0ffeec37e89edb2a93e13077
2396521990 <2396521990@qq.com>
2025-11-22 21:56:22 +0800

    更改平台设置，重新生成光照贴图

--- Assets/Scripts/UI/UI_Mission.cs
+++ Assets/Scripts/UI/UI_Mission.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class UI_Mission : UIBase
 {
